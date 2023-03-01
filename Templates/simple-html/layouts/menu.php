f9bd201c4ab159f8ce2d16b9b81e16536c1b7b5e
Maksym Haivoronskyi <maksym.haivoronskyi@hoststar.ch>
2023-03-01 16:59:47 +0100

    - changed view for menu

--- Templates/simple-html/layouts/menu.php
+++ Templates/simple-html/layouts/menu.php
@@ -1,13 +1,9 @@
 <?php
-$current_page = Core\App::$request->fullUrl();
 $controller = Core\App::$route->getController();
 $action = Core\App::$route->getAction();
 $pattern = Core\App::$route->getRoutePatern();
 
-//dump($pattern, $current_page, $controller, $action);
-//dump(preg_match($pattern, "/some-examples/?test_int=5&test_double=4&test_string1=as_view"));
-
-$menu1 = [
+$menu = [
     'Index' => "/",
     'Some usage example 1(response as View)' => "/some-examples?test_int=5&test_double=4&test_string1=as_view",
     'Some usage example 1(response as String)' => "/some-examples?test_int=5&test_double=4&test_string1=as_string",
@@ -18,37 +14,47 @@ $menu1 = [
     'phpinfo(), example route' => "/info2/any1/any2?any3=any4&any5=...",
     'echo, example route' => "/echo/any1/any2?any3=any4&any5=...",
     'web-console' => "/web-console/web-console-runner.php",
+
+    'Invoices Rest-Api' => [
+        'list' => "/invoice-api-usage",
+        'item(2)' => "/invoice-api-usage/2",
+        'Pdf for item(2)' => "/invoice-api-usage/2/pdf",
+        'Example fail Auth' => "/invoice-api-usage/2?bearer=ddddd",
+        'Example wrong Method' => "/invoice-api-usage/2/edit",
+        'Example wrong Url' => "/invoice-api-usage/2/not-exist",
+    ],
 ];
 
-$menu2 = [
-    'list' => "/invoice-api-usage",
-    'item(2)' => "/invoice-api-usage/2",
-    'Pdf for item(2)' => "/invoice-api-usage/2/pdf",
-    'Example fail Auth' => "/invoice-api-usage/2?bearer=ddddd",
-    'Example wrong Method' => "/invoice-api-usage/2/edit",
-    'Example wrong Url' => "/invoice-api-usage/2/not-exist",
-];
+/**
+ * Draw menu
+ * @param array $data
+ * @return void
+ */
+function showMenu(array $data)
+{
+    $current_page = Core\App::$request->fullUrl();
+    foreach ($data as $name => $url) {
+        if (is_array($url)) {
+            ?>
+            <li><?= $name ?>
+                <br>
+                <ul>
+                    <?php showMenu($url) ?>
+                </ul>
+            </li>
+            <?php
+        } else {
+            if ($current_page === $url) { $vars['title'] = $name; }
+            ?>
+            <li><a class="<?= ($current_page === $url)  ? 'selected' : '' ?>" href="<?= $url ?>"><?= $name ?></a></li>
+            <?php
+        }
+    }
+}
 ?>
+
 <ul>
     <?php
-    foreach ($menu1 as $name => $url) {
-        if ($current_page === $url) { $vars['title'] = $name; }
-        ?>
-        <li><a class="<?= ($current_page === $url)  ? 'selected' : '' ?>" href="<?= $url ?>"><?= $name ?></a></li>
-        <?php
-    }
+    showMenu($menu);
     ?>
-    <li>Invoices Rest-Api
-        <br>
-        <ul>
-            <?php
-            foreach ($menu2 as $name => $url) {
-                if ($current_page === $url) { $vars['title'] = $name; }
-                ?>
-                <li><a class="<?= ($current_page === $url)  ? 'selected' : '' ?>" href="<?= $url ?>"><?= $name ?></a></li>
-                <?php
-            }
-            ?>
-        </ul>
-    </li>
 </ul>
