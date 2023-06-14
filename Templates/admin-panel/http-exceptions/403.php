ed4e115142643d3529724ab532ae84200e4cc46b
Maksym Haivoronskyi <maksym.haivoronskyi@hoststar.ch>
2023-06-14 21:41:27 +0200

    - added error-templates for admin-panel

---
+++ Templates/admin-panel/http-exceptions/403.php
@@ -0,0 +1,8 @@
+<?php
+/** @var Core\ViewDriver $view */
+
+$vars['title'] = isset($vars['code']) ? $vars['code'] : '';
+?>
+<div class="ad-panel-content active">
+    <h2 style="color: #9a1a00">{%code} {%error}</h2>
+</div>
