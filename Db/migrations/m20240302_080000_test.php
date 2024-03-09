a83c6c69d8d3ee95c5d3a3116c2b51713fa3f069
Maksym Haivoronskyi (Home) <maksym.haivoronskyi@hoststar.ch>
2024-03-09 22:48:54 +0100

    - working on universalization for migration system (to create, update table by the same code for different db-types instead executing direct-sql-statement)

--- Db/migrations/m20240302_080000_test.php
+++ Db/migrations/m20240302_080000_test.php
@@ -23,7 +23,7 @@ class m20240302_080000_test extends mMain
      */
     public function up()
     {
-        return $this->Schema->createTableIfNotExists('{{tesssssssssst}}', function (SchemaTable $table) {
+        return $this->Schema->createTable/*IfNotExists*/('{{tesssssssssst}}', function (SchemaTable $table) {
             $table->column('id')->int(8)->primaryKey()->comment('test \' comment')->unsigned()->nullable(false);
             $table->column('user_id')->int()->comment('test foreign');//->unsigned()->nullable(false);
             $table->column('email')->string(50)->unique()->defaultVal('test_email')->nullable(false)->comment('user email')->unique();
@@ -34,6 +34,14 @@ class m20240302_080000_test extends mMain
             $table->column('decimal_test')->decimal()->unsigned()->nullable(false)->defaultVal(13.891);
             $table->column('bit_test')->bit(4)->unsigned()->nullable(false)->defaultVal(3);
             $table->column('bool_test')->bool()->unsigned()->nullable(false)->defaultVal(55)->index();
+            $table->column('text_test')->text()->defaultVal('text')->nullable(false);
+            $table->column('blob_test')->blob()->defaultVal('text')->nullable(false);
+            $table->column('date_test')->date()->defaultVal('2024-12-05')->nullable(false);
+            $table->column('time_test')->time()->defaultVal('22:15:33')->nullable(false);
+            $table->column('datetime_test')->datetime()->defaultVal('2024-12-05 22:15:33')->nullable(false);
+            $table->column('timestamp_test')->timestamp()->defaultVal('2024-12-05 22:15:33')->nullable(false);
+            $table->column('year_test')->year()->defaultVal(2023)->nullable(false);
+            $table->column('json_test')->nullable(false)->defaultVal('dddd')->manual_raw('json NOT NULL');
             $table->unique('idx_name', ['name', 'reserved']);
             $table->index('idx_name', 'name');
             $table->foreignKey('idx_foreign_user_id', 'user_id', '{{users}}', 'id', 'SET NULL', 'CASCADE');
