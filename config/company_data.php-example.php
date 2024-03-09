ca14f4a68bfa156485a26bced4b88b3d751f4457
Maksym Haivoronskyi (Home) <maksym.haivoronskyi@hoststar.ch>
2024-03-09 01:05:46 +0100

    - upgrade framework config folder with config-files   now folder has several different config-files which connect then in main.php

---
+++ config/company_data.php-example.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'version_invoice_pdf' => 'weasyprint', // weasyprint | tcpdf
+
+    'chilkat' => [
+        'paymentUrl' => "https://payment.webland.ch",
+        'WeblandGlobalLicense' => '',
+        'pdfBillHexKey' => '',
+        'pdfBillHexIV' => '',
+        'iconsBannerFolder' => __DIR__ . '/../../assets/img/banner-payment/',
+    ],
+];
