60d956466867bb29ced5c44a606e256e6eee25cf
Maksym Haivoronskyi <maksym.haivoronskyi@hoststar.ch>
2024-04-10 16:06:17 +0200

    - small fix for LogDriver

--- www/index.php
+++ www/index.php
@@ -15,9 +15,6 @@ try {
     /** connect autoload */
     require_once(__DIR__ . '/../vendor/autoload.php');
 
-    /** for web-app disable in LogDriver ob_end_flush()  */
-    Core\LogDriver::$execute_ob_end_flush = false;
-
     /** init and run application */
     Core\App::init(__DIR__ . "/../config")->run();
 
