b91f78a3ce39942ed0f45b1cf5da3fc22b6a73b9
Maksym Haivoronskyi <maksym.haivoronskyi@hoststar.ch>
2024-04-10 08:50:42 +0200

    - upgrade for mutators created_at and updated_at

--- Templates/admin-panel/pages/dashboard.php
+++ Templates/admin-panel/pages/dashboard.php
@@ -4,14 +4,13 @@
 
 $vars['title'] = __('Dashboard');
 $vars['new_contacts_count'] = $new_contacts_count;
-
 ?>
 <div class="ad-panel-content active">
     <h2><?= $vars['title'] ?></h2>
     <div class="narrow-center">
+        <p><?= __('Current date (UTC): {%date}', ['date' => gmdate("Y-m-d H:i:s")])?></p>
         <p></p>
         <p><?= __('You have:') ?></p>
         <p><?= __(' - <b>{%count}</b> new Message(s)', ['count' => $new_contacts_count]) ?></p>
     </div>
 </div>
-
