18f3014e2e77309df2ee3ae0ed79df3974afa537
Maksym Haivoronskyi <maksym.haivoronskyi@hoststar.ch>
2025-10-29 22:06:28 +0100

    updates for debug-panel

--- Middleware/ResponseDebugPanel.php
+++ Middleware/ResponseDebugPanel.php
@@ -6,6 +6,7 @@ use Core\App;
 use Core\Interfaces\MiddlewareInterface;
 use Core\RequestDriver;
 use Core\ResponseDriver;
+use Maksym\Config\ConfigException;
 
 /**
  * Force answer as json
@@ -24,10 +25,15 @@ class ResponseDebugPanel implements MiddlewareInterface
     /**
      * @param ResponseDriver $response
      * @return void
+     * @throws ConfigException
      */
     public function handleOnResponse(ResponseDriver $response)
     {
-        if (config('SHOW_DEBUG_PANEL', false) && config('IS_DEBUG', false) && $response->isHtml() && App::$debug) {
+        if (App::$debug &&
+            config('SHOW_DEBUG_PANEL', false) &&
+            //config('IS_DEBUG', false) &&
+            $response->isHtml())
+        {
             /* if SHOW_DEBUG_PANEL=true and IS_DEBUG=true then will be showed special debug panel with all debug info */
             $response->setBody(
                 str_replace(
