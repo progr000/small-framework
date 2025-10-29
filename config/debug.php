576f8cc6b9fba32307280e86e5cddc533aa0106f
Maksym Haivoronskyi <maksym.haivoronskyi@hoststar.ch>
2025-10-29 14:49:47 +0100

    removed deprecated param from config

--- config/debug.php
+++ config/debug.php
@@ -3,9 +3,6 @@ return [
 
     /* handler 404 */
     'OWN_404_HANDLER' => true, // if this framework used as part of another project, you should set this parameter to false
-    /* wget params */
-    'IGNORE_SSL_ERRORS' => true, // if you planed sent request to the servers with wrong certificate need set to true
-
 
     /* debug mode */
     'IS_DEBUG' => true,
